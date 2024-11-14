810a5496c6e025805e657594ec3bcbba8cc7b8f8
Tatiana Freiberger Neiva <104945081+ProfTatiana@users.noreply.github.com>
2024-11-14 11:32:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function() {
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPassoId = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = document.getElementById(proximoPassoId);
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+        if (proximoPasso) {  // Verifica se o próximo passo existe
+            atual.classList.remove('ativo');
+            proximoPasso.classList.add('ativo');
+        } else {
+            console.warn(`O próximo passo com ID ${proximoPassoId} não existe.`);
+        }
+    });
+});
